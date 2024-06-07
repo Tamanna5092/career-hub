3a4ed981a97883a5656e0320c2abbfae1dc04e6e
Tamanna@5092 <tamannaaktermoni51@gmail.com>
2024-06-08 00:34:54 +0600

    job details

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import { CiDollar, CiLocationOn, CiMail } from "react-icons/ci";
+import { FaPhoneAlt } from 'react-icons/fa';
+import { IoBagOutline } from 'react-icons/io5';
 
 const JobDetails = () => {
     const jobs = useLoaderData()
@@ -9,10 +12,30 @@ const JobDetails = () => {
     console.log(job)
     return (
         <div>
-            <h1>job details: {id}</h1>
+            <h1>Job Details</h1>
             <div className='grid gap-6 md:grid-cols-4'>
-                <div className='border-4 md:col-span-3'></div>
-                <div className='border-4'></div>
+                <div className='border-4 md:col-span-3'>
+                    <p>Job Description: {job.job_description}</p>
+                    <p>Job Responsibility: {job.job_responsibility}</p>
+                    <p>Education Requirement: {job.educational_requirements}</p>
+                    <p>Experience: {job.experiences}</p>
+                </div>
+                <div className='border-4'>
+                    <div>
+                    <h3>Job Details</h3>
+                    <hr />
+                    <p className='flex'><CiDollar className='text-2xl'></CiDollar>{job.salary}</p>
+                    <p className='flex'><IoBagOutline className='text-2xl'></IoBagOutline> Job Title: {job.job_title}</p>
+                    <h3>Contact Information</h3>
+                    <hr />
+                    <p className='flex'><FaPhoneAlt className='text-2xl'></FaPhoneAlt>Phone: {job.contact_information.phone}</p>
+                    <p className='flex'><CiMail className='text-2xl'></CiMail> Email: {job.contact_information.email}</p>
+                    <p className='flex'><CiLocationOn className='text-2xl'></CiLocationOn>Address: {job.contact_information.address}</p>
+                    </div>
+                    <div>
+                    <button className='btn w-full btn-primary'>Apply Now</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
