2841dd431b4716c788519cd77ff63c0b01073e5b
Tamanna@5092 <tamannaaktermoni51@gmail.com>
2024-06-10 13:29:31 +0600

    applied job

--- src/components/AppliedJobs.jsx
+++ src/components/AppliedJobs.jsx
@@ -1,11 +1,89 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { getStoredJobApplication } from "../utility/localstorage";
 
 const AppliedJobs = () => {
-    return (
-        <div>
-            <h1>I applied for this Job</h1>
-        </div>
-    );
+  const jobs = useLoaderData();
+  const [appliedJobs, setAppliedJobs] = useState([]);
+  const [displayJobs, setDisplayJobs] = useState([]);
+
+  const handleJobsFilter = filter => {
+    if(filter === 'all'){
+        setDisplayJobs(appliedJobs)
+    }
+    else if(filter === 'remote'){
+        const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote');
+        setDisplayJobs(remoteJobs);
+    }
+    else if(filter === 'onsite'){
+        const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite');
+        setDisplayJobs(onsiteJobs)
+    }
+  }
+
+  useEffect(() => {
+    const storedJobIds = getStoredJobApplication();
+    if (jobs.length > 0) {
+      // const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id))
+
+      const jobsApplied = [];
+      for (const id of storedJobIds) {
+        const job = jobs.find((job) => job.id === id);
+        if (job) {
+          jobsApplied.push(job);
+        }
+      }
+      setAppliedJobs(jobsApplied);
+      setDisplayJobs(jobsApplied)
+      // console.log(jobs, jobsApplied, jobsApplied)
+    }
+  }, [jobs]);
+
+  return (
+    <div>
+      <h1 className="text-2xl text-center">
+        Applied Jobs: {appliedJobs.length}
+      </h1>
+      <details className="dropdown">
+        <summary className="m-1 btn">open or close</summary>
+        <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+          <li onClick={() => handleJobsFilter('all')}>
+            <a>All</a>
+          </li>
+          <li onClick={() => handleJobsFilter('remote')}>
+            <a>Remote</a>
+          </li>
+          <li onClick={() => handleJobsFilter('onsite')}>
+            <a>Onsite</a>
+          </li>
+        </ul>
+      </details>
+      <ul>
+        {displayJobs.map((job) => (
+          <li key={job.id}>
+            <span>
+              {job.job_title} {job.company_name} : {job.remote_or_onsite}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+    // <div className="hero h-fit bg-base-200">
+    //   <div className="hero-content flex-col lg:flex-row">
+    //     <img
+    //       src={appliedJobs.logo}
+    //       className="max-w-sm rounded-lg shadow-2xl"
+    //     />
+    //     <div>
+    //       <h1 className="text-5xl font-bold">{title}</h1>
+    //       <p className="py-6">
+    //         Provident
+    //       </p>
+    //       <button className="btn btn-primary">Get Started</button>
+    //     </div>
+    //   </div>
+    // </div>
+  );
 };
 
 export default AppliedJobs;
--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -3,39 +3,49 @@ import { useLoaderData, useParams } from 'react-router-dom';
 import { CiDollar, CiLocationOn, CiMail } from "react-icons/ci";
 import { FaPhoneAlt } from 'react-icons/fa';
 import { IoBagOutline } from 'react-icons/io5';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { saveJobApplication } from '../utility/localstorage';
 
 const JobDetails = () => {
     const jobs = useLoaderData()
     const {id} = useParams()
     const idInt = parseInt(id)
     const job = jobs.find(job => job.id === idInt)
-    console.log(job)
+    // console.log(job)
+
+    const handleApplyJob = () =>{
+        saveJobApplication(idInt)
+        toast('You have applied successfully!')
+    }
+
     return (
         <div>
             <h1 className='text-3xl font-extrabold text-center'>Job Details</h1>
-            <div className='grid gap-6 md:grid-cols-4'>
+            <div className='grid gap-6 md:grid-cols-5'>
                 <div className='border-4 md:col-span-3 text-[#757575] space-y-6'>
                     <p className='font-medium'><span className='text-black font-extrabold'>Job Description:</span> {job.job_description}</p>
                     <p className='font-medium'><span className='text-black font-extrabold'>Job Responsibility:</span> {job.job_responsibility}</p>
                     <p className='font-medium'><span className='text-black font-extrabold'>Education Requirement:</span> <br /> <br /><span>{job.educational_requirements}</span></p>
                     <p className='font-medium'><span className='text-black font-extrabold'>Experience: <br /> <br /></span> {job.experiences}</p>
                 </div>
-                <div className='border-4 h-fit'>
-                    <div className='bg-gradient-to-r from-[#7E90FE1A] to-[#9873FF1A] text-[#757575] p-4'>
+                <div className='border-4 md:col-span-2 h-fit'>
+                    <div className='bg-gradient-to-r from-[#7E90FE1A] to-[#9873FF1A] text-[#757575] p-8'>
                     <h3 className='text-xl text-black font-extrabold'>Job Details</h3>
                     <hr />
                     <p className='flex font-medium mt-6  mb-4'><CiDollar className='text-2xl mr-2'></CiDollar><span className='text-black font-extrabold'>Salary: </span> <span>{job.salary}</span>(Per Month)</p>
-                    <p className='flex'><IoBagOutline className='text-2xl mr-2'></IoBagOutline> <span className='text-black font-extrabold'>Job Title:</span> {job.job_title}</p>
+                    <p className='flex'><IoBagOutline className='text-2xl mr-2'></IoBagOutline> <span className='text-black font-extrabold'>Job Title: </span> {job.job_title}</p>
                     <h3 className='text-xl text-black font-extrabold mt-8 mb-6'>Contact Information</h3>
                     <hr />
-                    <p className='flex mt-6'><FaPhoneAlt className='text-2xl mr-2'></FaPhoneAlt><span className='text-black font-extrabold'>Phone:</span> {job.contact_information.phone}</p>
-                    <p className='flex my-4'><CiMail className='text-2xl mr-2'></CiMail> <span className='text-black font-extrabold'>Email:</span> {job.contact_information.email}</p><span className='text-black font- extrabold'><span className='text-black font-extrabold'>Address:</span></span>
-                    <p className='flex'><CiLocationOn className='text-2xl'></CiLocationOn> {job.contact_information.address}</p>
+                    <p className='flex mt-6'><FaPhoneAlt className='text-2xl mr-2'></FaPhoneAlt><span className='text-black font-extrabold'>Phone: </span> {job.contact_information.phone}</p>
+                    <p className='flex my-4'><CiMail className='text-2xl mr-2'></CiMail> <span className='text-black font-extrabold'>Email: </span> {job.contact_information.email}</p>
+                    <p className='flex'><CiLocationOn className='text-2xl mr-2'></CiLocationOn><span className='text-black font-extrabold'>Address: </span> {job.contact_information.address}</p>
                     </div>
                     <div>
-                    <button className='btn w-full mt-6 bg-gradient-to-r from-[#7E90FE] to-[#9873FF]'>Apply Now</button>
+                    <button onClick={handleApplyJob} className='btn w-full mt-6 bg-gradient-to-r from-[#7E90FE] to-[#9873FF]'>Apply Now</button>
                     </div>
                 </div>
+                <ToastContainer />
             </div>
         </div>
     );
